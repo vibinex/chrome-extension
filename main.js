95df6163caf31622673f0f3e4637310f0a553172
ajayCreations <ajay.yadav@vibinex.com>
2023-03-10 11:46:59 +0530

    added css for important and relevant according to data and removed unused css

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
-
-let url = window.location.toString(); 
-console.log('This is the url',url)
-
-
-function ElementCreator(elementId,title,priority){
-  const newElement = document.createElement("span");
-  const  addTitle = document.createTextNode(title);
-  newElement.appendChild(addTitle);
-  newElement.setAttribute('id',priority);
-
-  const finalElement = document.getElementById(elementId);
-  finalElement.appendChild(newElement);
-}
-
-
-function AddingCSS(elementId,title,priority){
-  one = document.head.appendChild(document.createElement("style"));
-  one.innerHTML = `#${elementId}::before{
+function addingImportantCss(elementId) {
+  document.getElementById(elementId).style.backgroundColor='rgb(61, 0, 0)';
+  let element = document.head.appendChild(document.createElement("style"));
+  element.innerHTML = `#${elementId}_link::before{
     background-color:red;
     content: 'Important';
     color: white;
@@ -28,28 +13,41 @@ function AddingCSS(elementId,title,priority){
     padding-left: 5px;
     padding-right: 5px;
     padding-bottom: 2px;;}`;
-}
+};
 
+function addingRelevantCss(elementId,) {
+  document.getElementById(elementId).style.backgroundColor='rgb(86, 88, 0)';
+  let element = document.head.appendChild(document.createElement("style"));
+  element.innerHTML = `#${elementId}_link::before{
+    background-color: rgb(164, 167, 0);
+    content: 'Relevant';
+    color: white;
+    width: 12px;
+    height: 12px;
+    border: rgb(45, 0, 0);
+    border-radius: 5px;
+    margin-right: 10px;
+    padding-left: 5px;
+    padding-right: 5px;
+    padding-bottom: 2px;;}`;
+};
 
+//sending url which contain repo name 
+// to do : getting user info for sending to backend
+let url = window.location.toString();
 
-const toCreate=[
-  {elementId:'issue_36401_link',title:'Medium',priority:'Medium'},
-  {elementId:'issue_36402_link',title:'Low',priority:'Low'},
-  {elementId:'issue_36408_link',title:'Low',priority:'Low'},
-  {elementId:'issue_36400_link',title:'Medium',priority:'Medium'},
-  {elementId:'issue_36404_link',title:'Low',priority:'Low'},
-  {elementId:'issue_36405_link',title:'Low',priority:'Low'},
-  {elementId:'issue_36407_link',title:'Low',priority:'Low'},
-  {elementId:'issue_36409_link',title:'Low',priority:'Low'},
-  {elementId:'issue_36410_link',title:'Low',priority:'Low'},
+// data getting from api 
+const output = [
+  {
+    important: ['issue_36485','issue_36458','issue_36462'],
+    relevant: ['issue_36460','issue_36483','issue_36466']
+  }
+];
 
-  {elementId:'issue_16_link',title:'High',priority:'High'},
-  {elementId:'issue_10_link',title:'High',priority:'High'},
-  {elementId:'issue_9_link',title:'High',priority:'High'},
-  {elementId:'issue_2_link',title:'Medium',priority:'Medium'},
-  {elementId:'issue_8_link',title:'High',priority:'High'},
-]
+output[0].important.map((item)=>{
+  addingImportantCss(item)
+});
 
-toCreate.map((item)=>{
-    AddingCSS(item.elementId);
-})
+output[0].relevant.map((item)=>{
+  addingRelevantCss(item)
+});
