2883cd00e0943312037fe1e16dc582b06682a4fc
ajayCreations <ajay.yadav@vibinex.com>
2023-03-11 08:33:40 +0530

    removed extra function, loop and done proper naming

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
-function addingImportantCss(elementId) {
-  document.getElementById(`issue_${elementId}`).style.backgroundColor='rgb(61, 0, 0)';
+function addingCssElement(elementId,status) {
+  let backgroundColor = status =='Important'?'rgb(61, 0, 0)':'rgb(86, 88, 0)';
+  let tagBackgroundColor = status =='Important'?'rgb(255,0,0)':'rgb(164, 167, 0)';
+  document.getElementById(`issue_${elementId}`).style.backgroundColor = backgroundColor;
   let element = document.head.appendChild(document.createElement("style"));
   element.innerHTML = `#issue_${elementId}_link::before{
-    background-color:red;
-    content: 'Important';
-    color: white;
-    width: 12px;
-    height: 12px;
-    border: rgb(45, 0, 0);
-    border-radius: 5px;
-    margin-right: 10px;
-    padding-left: 5px;
-    padding-right: 5px;
-    padding-bottom: 2px;;}`;
-};
-
-function addingRelevantCss(elementId,) {
-  document.getElementById(`issue_${elementId}`).style.backgroundColor='rgb(86, 88, 0)';
-  let element = document.head.appendChild(document.createElement("style"));
-  element.innerHTML = `#issue_${elementId}_link::before{
-    background-color: rgb(164, 167, 0);
-    content: 'Relevant';
+    background-color:${tagBackgroundColor};
+    content: '${status}';
     color: white;
     width: 12px;
     height: 12px;
@@ -37,17 +22,14 @@ function addingRelevantCss(elementId,) {
 let url = window.location.toString();
 
 // data getting from api 
-const output = [
-  {
-    important: [36485,36458,36462],
-    relevant: [36460,36483,36466]
-  }
-];
+const highlightedPRIds = {
+  important: [36485, 36458, 36462,36492],
+  relevant: [36460, 36483, 36466]
+};
 
-output[0].important.map((item)=>{
-  addingImportantCss(item)
-});
+let maxLengthIds = Math.max(highlightedPRIds.important.length,highlightedPRIds.relevant.length);
 
-output[0].relevant.map((item)=>{
-  addingRelevantCss(item)
-});
+for(let i=0;i<maxLengthIds;i++){
+  highlightedPRIds.important[i]?addingCssElement(highlightedPRIds.important[i],'Important'):null;
+  highlightedPRIds.relevant[i]?addingCssElement(highlightedPRIds.relevant[i],'relevant'):null;
+}
