20b04c02ffb7833afd80f8baaa8877331e7770d0
ajayCreations <ajay.yadav@vibinex.com>
2023-04-01 15:54:12 +0530

    Inital highilighting file in pr for bitbucket

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -246,13 +246,13 @@ function addCssElementToBitbucket(highlightedPRIds) {
 
 // adding css elements based up the data getting from api
 function getHighlightedPR(highlightedPRIds) {
-		if (highlightedPRIds) {
-			for (const priorityLevel in highlightedPRIds) {
-				for (const prNumber in highlightedPRIds[priorityLevel]) {
-					addingCssElementToGithub(prNumber, keyToLabel[priorityLevel], highlightedPRIds[priorityLevel][prNumber]['num_files_changed'])
-				}
+	if (highlightedPRIds) {
+		for (const priorityLevel in highlightedPRIds) {
+			for (const prNumber in highlightedPRIds[priorityLevel]) {
+				addingCssElementToGithub(prNumber, keyToLabel[priorityLevel], highlightedPRIds[priorityLevel][prNumber]['num_files_changed'])
 			}
 		}
+	}
 };
 
 // adding favButton
@@ -265,23 +265,66 @@ async function showFloatingActionButton(orgName, orgRepo, userId, websiteUrl) {
 
 // showing the important files in a pr
 async function showImpFileInPr(response) {
-		if ("relevant" in response) {
-			const encryptedFileNames = new Set(response['relevant']);
-			const fileNav = document.querySelector('[aria-label="File Tree Navigation"]');
-			if (!fileNav) return;
-			const fileList = Array.from(fileNav.getElementsByTagName('li'));
-			fileList.forEach(async (item) => {
-				let elements = item.getElementsByClassName('ActionList-item-label');
-				if (elements.length == 1) {
-					let filename = elements[0].innerHTML.trim();
-					const hashedFilename = await sha256(filename);
-					if (encryptedFileNames.has(hashedFilename)) {
-						item.style.backgroundColor = '#7a7e00';
+	if ("relevant" in response) {
+		const encryptedFileNames = new Set(response['relevant']);
+		const fileNav = document.querySelector('[aria-label="File Tree Navigation"]');
+		if (!fileNav) return;
+		const fileList = Array.from(fileNav.getElementsByTagName('li'));
+		fileList.forEach(async (item) => {
+			let elements = item.getElementsByClassName('ActionList-item-label');
+			if (elements.length == 1) {
+				let filename = elements[0].innerHTML.trim();
+				const hashedFilename = await sha256(filename);
+				if (encryptedFileNames.has(hashedFilename)) {
+					item.style.backgroundColor = '#7a7e00';
+				}
+			}
+		})
+	}
+}
+
+// highlighting the files in pr for bitbucket 
+async function FilesInPrBitbucket(response) {
+	let lastKnownScrollPosition = 0;
+	let currentScrollPosition = 0;
+	let ticking = false;
+	document.addEventListener('scroll', () => {
+		currentScrollPosition = window.scrollY;
+		if (!ticking) {
+			window.requestAnimationFrame(() => {
+				if (currentScrollPosition - lastKnownScrollPosition > 100) {
+					if ("relevant" in response) {
+						const encryptedFileNames = new Set(response['relevant']);
+						console.log(`[scroll] ticking: ${ticking}; currentScrollPosition: ${currentScrollPosition}, lastKnownScrollPosition: ${lastKnownScrollPosition}`)
+						const fileNav = Array.from(document.querySelectorAll("[aria-label^='Diff of file']"))
+						lastKnownScrollPosition = currentScrollPosition;
+						fileNav.forEach(async (element) => {
+							const h3Element = element.querySelector('h3');
+							const spanElement = Array.from(h3Element.querySelectorAll('span'));
+							const elementHeading = spanElement.length == 1 ? spanElement[0] : spanElement[spanElement.length - 1];
+							const spanText = elementHeading.textContent;
+
+							let hashFileName = await sha256(spanText);
+							if (encryptedFileNames.includes(hashFileName)) {
+								if (spanElement.length == 1) {
+									let changeBgColor = element.getElementsByClassName('css-10sfmq2')[0];
+									changeBgColor.style.backgroundColor = '#c5cc02';
+								} else {
+									let value = elementHeading.parentNode.parentNode.parentNode.parentNode.parentNode;
+									let value2 = value.children[0];
+									value2.style.backgroundColor = '#c5cc02';
+								}
+							}
+						})
 					}
 				}
-			})
+				ticking = false;
+			});
+			ticking = true;
 		}
-	}
+	});
+}
+
 
 const orchestrator = (tab_url, websiteUrl, userId) => {
 	console.debug(`[vibinex-orchestrator] updated url: ${tab_url}`);
@@ -291,15 +334,15 @@ const orchestrator = (tab_url, websiteUrl, userId) => {
 		// TODO: create a UI element on the screen with CTA to login to Vibinex
 	}
 	chrome.storage.sync.get(["backendUrl"]).then(async ({ backendUrl }) => {
-	if (urlObj[2] == 'github.com') {
-		if (urlObj[3] && (urlObj[3] !== 'orgs') && urlObj[4]) {
-			// for showing fav button if org repo is not added, eg : https://github.com/mui/mui-toolpad
-			const owner_name = urlObj[3];
-			const repo_name = urlObj[4];
-			showFloatingActionButton(owner_name, repo_name, userId, websiteUrl);
+		const owner_name = urlObj[3];
+		const repo_name = urlObj[4];
+		if (urlObj[2] == 'github.com') {
+			if (urlObj[3] && (urlObj[3] !== 'orgs') && urlObj[4]) {
+				// for showing fav button if org repo is not added, eg : https://github.com/mui/mui-toolpad
+				showFloatingActionButton(owner_name, repo_name, userId, websiteUrl);
 
-			if (urlObj[5] === 'pulls') {
-				// show relevant PRs
+				if (urlObj[5] === 'pulls') {
+					// show relevant PRs
 					const body = {
 						"repo_owner": owner_name,
 						"repo_name": repo_name,
@@ -309,9 +352,9 @@ const orchestrator = (tab_url, websiteUrl, userId) => {
 					const url = `${backendUrl}/relevance/pr`;
 					const highlightedPRIds = await apiCall(url, body);
 					getHighlightedPR(highlightedPRIds);
-			}
-			if (urlObj[5] === "pull" && urlObj[6] && urlObj[7] === "files") {
-				const pr_number = urlObj[6];
+				}
+				if (urlObj[5] === "pull" && urlObj[6] && urlObj[7] === "files") {
+					const pr_number = urlObj[6];
 					const body = {
 						"repo_owner": owner_name,
 						"repo_name": repo_name,
@@ -322,28 +365,43 @@ const orchestrator = (tab_url, websiteUrl, userId) => {
 					const url = `${backendUrl}/relevance/pr/files`;
 					let response = await apiCall(url, body);
 					showImpFileInPr(response);
+				}
+			}
+			// for showing all tracked repo
+			else if (
+				(urlObj[3] && urlObj[4] == undefined) ||
+				(urlObj[3] == 'orgs' && urlObj[4] && urlObj[5] === 'repositories')) {
+				// for woking on this url https://github.com/Alokit-Innovations or https://github.com/orgs/Alokit-Innovations/repositories?type=all type 
+				const org_name = (urlObj[3] === "orgs") ? urlObj[4] : urlObj[3];
+				updateTrackedReposInOrgGitHub(org_name, websiteUrl, userId);
 			}
 		}
-		// for showing all tracked repo
-		else if (
-			(urlObj[3] && urlObj[4] == undefined) ||
-			(urlObj[3] == 'orgs' && urlObj[4] && urlObj[5] === 'repositories')) {
-			// for woking on this url https://github.com/Alokit-Innovations or https://github.com/orgs/Alokit-Innovations/repositories?type=all type 
-			const org_name = (urlObj[3] === "orgs") ? urlObj[4] : urlObj[3];
-			updateTrackedReposInOrgGitHub(org_name, websiteUrl, userId);
-		}
-	}
 
-	if (urlObj[2] === "bitbucket.org" && urlObj[5] === "pull-requests") {
+		if (urlObj[2] === "bitbucket.org" && urlObj[5] === "pull-requests") {
+
 			const body = {
-				"repo_owner": urlObj[3],
-				"repo_name": urlObj[4],
+				"repo_owner": owner_name,
+				"repo_name": repo_name,
 				"user_id": userId,
 				"is_github": false
 			}
 			const url = `${backendUrl}/relevance/pr`;
 			let highlightedPRIds = await apiCall(url, body);
 			addCssElementToBitbucket(highlightedPRIds);
+
+			if (urlObj[6]) {
+				const pr_number = urlObj[6];
+				const body = {
+					"repo_owner": owner_name,
+					"repo_name": repo_name,
+					"user_id": userId,
+					"pr_number": pr_number,
+					"is_github": false
+				}
+				const url = `${backendUrl}/relevance/pr/files`;
+				let response = await apiCall(url, body);
+				FilesInPrBitbucket(response);
+			}
 		}
 	})
 };
