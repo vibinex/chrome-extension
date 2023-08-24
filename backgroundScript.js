f6ad01d6a1348166edaabc2bfaf9568e96109afe
Tapish Rathore <tapish303@gmail.com>
2023-08-24 19:26:17 +0530

    fix host permissions and syncs in background script

--- backgroundScript.js
+++ backgroundScript.js
@@ -3,10 +3,10 @@ chrome.runtime.onInstalled.addListener(() => {
 	// const environment = "dev";
 	const websiteUrl = (environment === "dev") ? "http://localhost:3000" : "https://vibinex.com";
 	const backendUrl = (environment === "dev") ? "http://localhost:8080" : "https://gcscruncsql-k7jns52mtq-el.a.run.app";
-	chrome.storage.sync.set({ websiteUrl, backendUrl }).then(_ => console.log(`Website URL set to ${websiteUrl};`));
+	chrome.storage.local.set({ websiteUrl, backendUrl }).then(_ => console.log(`Website URL set to ${websiteUrl};`));
 
 	// API call to backend that created Rudderstack event
-	chrome.storage.sync.get(["userId"]).then(({ userId }) => {
+	chrome.storage.local.get(["userId"]).then(({ userId }) => {
 		const body = {
 			userId: userId ? userId : "anonymous-id",
 			function: 'chrome-extension-installed'
