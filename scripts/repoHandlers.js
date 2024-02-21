1481febb129d802e87bc2fc52b0004a6a275c4a5
Muskan Paliwal <muskan10112002@gmail.com>
2024-02-21 14:37:55 +0530

    remove the indentation fixes

--- scripts/repoHandlers.js
+++ scripts/repoHandlers.js
@@ -7,24 +7,24 @@
  * @returns {Array} - List of tracked repositories.
  */
 async function getTrackedRepos(orgName, userId, repoHost) {
-    const { websiteUrl } = await chrome.storage.local.get(["websiteUrl"]);
-    let body = {};
-    let url = ''
-    switch (repoHost) {
-        case 'github':
-            body = { org: orgName, userId: userId, provider: 'github' }
-            url = `${websiteUrl}/api/extension/setup`;
-            break;
-        case 'bitbucket':
-            body = { org: orgName, userId: userId, provider: 'bitbucket' }
-            url = `${websiteUrl}/api/extension/setup`;
-            break;
-        default:
-            console.warn(`[getTrackedRepos] Invalid repoHost provided: ${repoHost}`);
-            break;
-    }
-    const trackedRepos = await apiCallOnprem(url, body);
-    return trackedRepos['repos'];
+	const { websiteUrl } = await chrome.storage.local.get(["websiteUrl"]);
+	let body = {};
+	let url = ''
+	switch (repoHost) {
+		case 'github':
+			body = { org: orgName, userId: userId, provider: 'github' }
+			url = `${websiteUrl}/api/extension/setup`;
+			break;
+		case 'bitbucket':
+			body = { org: orgName, userId: userId, provider: 'bitbucket' }
+			url = `${websiteUrl}/api/extension/setup`;
+			break;
+		default:
+			console.warn(`[getTrackedRepos] Invalid repoHost provided: ${repoHost}`);
+			break;
+	}
+	const trackedRepos = await apiCallOnprem(url, body);
+	return trackedRepos['repos'];
 }
 
 /**
@@ -34,34 +34,34 @@ async function getTrackedRepos(orgName, userId, repoHost) {
  * @param {string} websiteUrl - The URL of the website.
  */
 function updateTrackedReposInBitbucketOrg(trackedRepos, websiteUrl) {
-    const tbody = document.querySelector('tbody');
-    const trs = tbody.querySelectorAll('td');
+	const tbody = document.querySelector('tbody');
+	const trs = tbody.querySelectorAll('td');
 
-    trs.forEach((item) => {
-        const text = Array.from(item.getElementsByTagName('a'));
-        if (text.length >= 2 && trackedRepos.includes(text[1].innerHTML)) {
-            const img = document.createElement("img");
-            img.setAttribute('class', 'trackLogo');
-            const beforePsuedoElement = document.createElement('a');
-            img.src = `${websiteUrl}/favicon.ico`;
-            img.style.width = '15px'
-            img.style.height = '15px'
-            img.style.marginBottom = '-3px'
-            img.style.marginRight = '3px'
+	trs.forEach((item) => {
+		const text = Array.from(item.getElementsByTagName('a'));
+		if (text.length >= 2 && trackedRepos.includes(text[1].innerHTML)) {
+			const img = document.createElement("img");
+			img.setAttribute('class', 'trackLogo');
+			const beforePsuedoElement = document.createElement('a');
+			img.src = `${websiteUrl}/favicon.ico`;
+			img.style.width = '15px'
+			img.style.height = '15px'
+			img.style.marginBottom = '-3px'
+			img.style.marginRight = '3px'
 
-            beforePsuedoElement.appendChild(img);
-            beforePsuedoElement.href = `${websiteUrl}/repo?repo_name=${text[1].innerHTML}`;
-            beforePsuedoElement.target = '_blank';
-            beforePsuedoElement.style.display = 'inline-block';
-            beforePsuedoElement.style.marginRight = '2px';
-            beforePsuedoElement.style.color = 'white';
-            beforePsuedoElement.style.borderRadius = '2px';
-            beforePsuedoElement.style.fontSize = '15px';
-            beforePsuedoElement.style.textDecoration = 'none';
-            text[1].insertBefore(beforePsuedoElement, text[1].firstChild)
-        }
+			beforePsuedoElement.appendChild(img);
+			beforePsuedoElement.href = `${websiteUrl}/repo?repo_name=${text[1].innerHTML}`;
+			beforePsuedoElement.target = '_blank';
+			beforePsuedoElement.style.display = 'inline-block';
+			beforePsuedoElement.style.marginRight = '2px';
+			beforePsuedoElement.style.color = 'white';
+			beforePsuedoElement.style.borderRadius = '2px';
+			beforePsuedoElement.style.fontSize = '15px';
+			beforePsuedoElement.style.textDecoration = 'none';
+			text[1].insertBefore(beforePsuedoElement, text[1].firstChild)
+		}
 
-    });
+	});
 }
 
 /**
@@ -71,14 +71,14 @@ function updateTrackedReposInBitbucketOrg(trackedRepos, websiteUrl) {
  * @returns {Array} - The array of repoUrl.
  */
 function githubNewUIRepoUrls(ownerType) {
-    const repoList = ownerType === 'org' ? document.querySelectorAll('[data-testid="list-view-items"] > li') : document.querySelectorAll('[data-filterable-for="your-repos-filter"] > li');
-    let repoUrls = [];
-    repoList.forEach(repoItem => {
-        let repoLink = ownerType == 'org' ? repoItem.querySelector('[data-testid="listitem-title-link"]') : repoItem.querySelector('a[itemprop="name codeRepository"]');
-        if (!repoLink) return;
-        repoUrls.push(repoLink);
-    });
-    return repoUrls;
+	const repoList = ownerType === 'org' ? document.querySelectorAll('[data-testid="list-view-items"] > li') : document.querySelectorAll('[data-filterable-for="your-repos-filter"] > li');
+	let repoUrls = [];
+	repoList.forEach(repoItem => {
+		let repoLink = ownerType == 'org' ? repoItem.querySelector('[data-testid="listitem-title-link"]') : repoItem.querySelector('a[itemprop="name codeRepository"]');
+		if (!repoLink) return;
+		repoUrls.push(repoLink);
+	});
+	return repoUrls;
 }
 
 /**
@@ -88,9 +88,9 @@ function githubNewUIRepoUrls(ownerType) {
  * @returns {Array} - The array of repoUrl.
  */
 function githubOldUIRepoUrls(ownerType) {
-    const allRepo = ownerType === 'org' ? document.getElementById('org-repositories') : document.getElementById('user-repositories-list');
-    const repoUrls = Array.from(allRepo.querySelectorAll('a[itemprop="name codeRepository"]'));
-    return repoUrls;
+	const allRepo = ownerType === 'org' ? document.getElementById('org-repositories') : document.getElementById('user-repositories-list');
+	const repoUrls = Array.from(allRepo.querySelectorAll('a[itemprop="name codeRepository"]'));
+	return repoUrls;
 }
 
 /**
@@ -101,42 +101,42 @@ function githubOldUIRepoUrls(ownerType) {
  * @param {boolean} isOrg - Whether github organization or user.
  */
 function updateTrackedReposInGitHub(trackedRepos, websiteUrl, ownerType) {
-    // Check if selectors for new UI exist
-    const newUI = document.querySelector('[data-testid="list-view-items"]') ? true : false
-    const repoUrls = newUI ? githubNewUIRepoUrls(ownerType) : githubOldUIRepoUrls(ownerType);
-    repoUrls.forEach(repoItem => {
-        const repoLink = repoItem.getAttribute('href');
-        const repoName = repoLink.split('/').pop();
-        
-        if (trackedRepos.includes(repoName)) {
-            const checkElement = repoItem.querySelector('.trackLogo');
-            if (checkElement) {
-                // TODO: Ideally, we should only need to add the element when there is none present
-                checkElement.remove();
-            }
+	// Check if selectors for new UI exist
+	const newUI = document.querySelector('[data-testid="list-view-items"]') ? true : false
+	const repoUrls = newUI ? githubNewUIRepoUrls(ownerType) : githubOldUIRepoUrls(ownerType);
+	repoUrls.forEach(repoItem => {
+		const repoLink = repoItem.getAttribute('href');
+		const repoName = repoLink.split('/').pop();
+		
+		if (trackedRepos.includes(repoName)) {
+			const checkElement = repoItem.querySelector('.trackLogo');
+			if (checkElement) {
+				// TODO: Ideally, we should only need to add the element when there is none present
+				checkElement.remove();
+			}
 
-            const img = document.createElement("img");
-            img.classList.add('trackLogo');
-            img.src = `${websiteUrl}/favicon.ico`;
-            img.style.width = '15px';
-            img.style.height = '15px';
+			const img = document.createElement("img");
+			img.classList.add('trackLogo');
+			img.src = `${websiteUrl}/favicon.ico`;
+			img.style.width = '15px';
+			img.style.height = '15px';
 
-            const vibinexLogoElement = document.createElement('a');
-            vibinexLogoElement.appendChild(img);
-            vibinexLogoElement.href = `${websiteUrl}/repo?repo_name=${repoName}`;
-            vibinexLogoElement.target = '_blank';
+			const vibinexLogoElement = document.createElement('a');
+			vibinexLogoElement.appendChild(img);
+			vibinexLogoElement.href = `${websiteUrl}/repo?repo_name=${repoName}`;
+			vibinexLogoElement.target = '_blank';
 
-            vibinexLogoElement.style.display = newUI ? 'inline-flex' : 'inline-block';
-            vibinexLogoElement.style.marginRight ='6px';
-            vibinexLogoElement.style.color = 'white';
-            vibinexLogoElement.style.borderRadius = '2px';
-            vibinexLogoElement.style.fontSize = '15px';
-            vibinexLogoElement.style.textDecoration = 'none';
-            vibinexLogoElement.style.alignItems ='center';
+			vibinexLogoElement.style.display = newUI ? 'inline-flex' : 'inline-block';
+			vibinexLogoElement.style.marginRight ='6px';
+			vibinexLogoElement.style.color = 'white';
+			vibinexLogoElement.style.borderRadius = '2px';
+			vibinexLogoElement.style.fontSize = '15px';
+			vibinexLogoElement.style.textDecoration = 'none';
+			vibinexLogoElement.style.alignItems ='center';
 
-            newUI ? repoItem.parentNode.insertBefore(vibinexLogoElement, repoItem) : repoItem.insertBefore(vibinexLogoElement, repoItem.firstChild);
-        }
-    });
+			newUI ? repoItem.parentNode.insertBefore(vibinexLogoElement, repoItem) : repoItem.insertBefore(vibinexLogoElement, repoItem.firstChild);
+		}
+	});
 }
 
 /**
@@ -149,8 +149,8 @@ function updateTrackedReposInGitHub(trackedRepos, websiteUrl, ownerType) {
  * @param {string} repoHost - The hosting platform ('github' or 'bitbucket').
  */
 async function showFloatingActionButton(orgName, orgRepo, userId, websiteUrl, repoHost) {
-    const trackedRepoList = await getTrackedRepos(orgName, userId, repoHost);
-    if (!trackedRepoList.includes(orgRepo)) {
-        createElement("add", websiteUrl);
-    }
+	const trackedRepoList = await getTrackedRepos(orgName, userId, repoHost);
+	if (!trackedRepoList.includes(orgRepo)) {
+		createElement("add", websiteUrl);
+	}
 }
