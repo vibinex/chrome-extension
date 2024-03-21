a603ff4bdd251bee4a1e3b76dba3566ede3b73a8
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-03-21 10:52:22 +0530

    removed backendUrl from everywhere because it is not used anymore

--- backgroundScript.js
+++ backgroundScript.js
@@ -4,12 +4,11 @@ chrome.runtime.onInstalled.addListener(() => {
 	const environment = "prod";
 	// const environment = "dev";
 
-	// Set the website and backend URLs based on the environment.
+	// Set the website URL based on the environment.
 	const websiteUrl = (environment === "dev") ? "http://localhost:3000" : "https://vibinex.com";
-	const backendUrl = (environment === "dev") ? "http://localhost:8080" : "https://gcscruncsql-k7jns52mtq-el.a.run.app";
 
-	// Store the website and backend URLs in Chrome's local storage.
-	chrome.storage.local.set({ websiteUrl, backendUrl }).then(_ => console.log(`Website URL set to ${websiteUrl};`));
+	// Store the website URL in Chrome's local storage.
+	chrome.storage.local.set({ websiteUrl }).then(_ => console.log(`Website URL set to ${websiteUrl};`));
 
 	// Make an API call to the backend to create a Rudderstack event when the extension is installed.
 	chrome.storage.local.get(["userId"]).then(({ userId }) => {
